0ff8e851b71158adcd142cfce04cc2d717bd7e5d
Paul <admin@paulschickmarket.com>
2020-12-20 22:45:46 -0500

    updated classes and styling for meteorite-details.component.html

--- src/app/meteorites/meteorite-detail.component.ts
+++ src/app/meteorites/meteorite-detail.component.ts
@@ -3,7 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'mt-detail',
   templateUrl: './meteorite-detail.component.html',
-  styles: [``]
+  styles: [`
+    .bold { font-weight: bold; }
+  `]
 })
 export class MeteoriteDetailComponent {
   @Input() meteorite:any
