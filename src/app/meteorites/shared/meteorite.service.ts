2ad7646d65c1185ad3fc6185cd4cfa5abd18ea18
Paul Schick <pschick@ceiamerica.com>
2020-12-22 16:15:05 -0500

    updated api endpoint to more recent API

--- src/app/meteorites/shared/meteorite.service.ts
+++ src/app/meteorites/shared/meteorite.service.ts
@@ -8,7 +8,7 @@ import { map, filter, catchError } from 'rxjs/operators';
 })
 export class MeteoriteService {
 
-  private meteoritesUrl:string = 'https://data.nasa.gov/resource/y77d-th95.json';
+  private meteoritesUrl:string = 'https://data.nasa.gov/resource/gh4g-9sfh.json';
 
   constructor(private http:HttpClient) {}
 
