e33dda766b39dea07bf49b0b93ea055153b463e6
Paul Schick <pschick@ceiamerica.com>
2020-12-24 13:58:14 -0500

    all instances of bootstrap removed
    application compiles, all bootstrap styling is removed, now can install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +17,6 @@ import { HomeModule } from './modules/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     HomeModule,
     CoreModule,
     HttpClientModule
