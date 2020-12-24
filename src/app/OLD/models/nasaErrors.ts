932bd16d2cfaf94c9300a8a2034081d6bb67d9c2
Paul Schick <pschick@ceiamerica.com>
2020-12-24 10:37:10 -0500

    successful compilation, old structure removed

--- src/app/OLD/models/nasaErrors.ts
+++
@@ -1,5 +0,0 @@
-export class NasaError {
-  errorNumber: number;
-  message: string;
-  additionalMessage: string;
-}
--- src/app/OLD/nasa-config.ts
+++
@@ -1,1 +0,0 @@
-export const NASA_API_KEY = 'wj7jAdpeOnwUhXeUR40W9Ll02P7hiJ2N0j5Rv0uY';
