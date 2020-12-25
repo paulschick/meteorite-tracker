0c108683ab5fa89fb706e9a66b886ee928c664c0
Paul <admin@paulschickmarket.com>
2020-12-25 10:10:03 -0500

    added sidenav/toolbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
 import { CoreModule } from './modules/core/core.module';
 import { HomeModule } from './modules/home/home.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,11 +15,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    ToolbarComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     HomeModule,
     CoreModule,
     HttpClientModule,
