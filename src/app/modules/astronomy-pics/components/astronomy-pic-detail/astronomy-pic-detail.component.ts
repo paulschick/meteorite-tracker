14a0c9b246b1ec3a2a8991b8faacc80415e81e36
Paul <admin@paulschickmarket.com>
2020-12-27 20:29:09 -0500

    render image properties on astronomy-pic detail component
    moved service injection to page file.
    detail component takes in Observable through Input property
    ngFor for one observable in array

--- src/app/modules/astronomy-pics/components/astronomy-pic-detail/astronomy-pic-detail.component.ts
+++ src/app/modules/astronomy-pics/components/astronomy-pic-detail/astronomy-pic-detail.component.ts
@@ -1,30 +1,20 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AstroPicsService } from '../../services/astro-pics.service';
-import { IApd } from '../../../../shared/models/apd.model';
-import { Subject } from 'rxjs';
-import { takeUntil, tap } from 'rxjs/operators';
+import { Component, OnInit, Input } from '@angular/core';
+import { IApd } from 'src/app/shared/models/apd.model';
+
 
 @Component({
   selector: 'mt-astronomy-pic-detail',
   templateUrl: './astronomy-pic-detail.component.html',
   styleUrls: ['./astronomy-pic-detail.component.scss']
 })
-export class AstronomyPicDetailComponent implements OnInit, OnDestroy {
+export class AstronomyPicDetailComponent implements OnInit {
 
-  detailImage: IApd[] = [];
-  private astronomyPicObs = new Subject();
+  @Input() astroDetailImage:IApd;
 
-  constructor(private astroPicsService: AstroPicsService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    var imageObservable = this.astroPicsService.getDetailImage('2020-12-26', this.detailImage);
-    imageObservable.pipe(takeUntil(this.astronomyPicObs)).subscribe()
-    console.log(this.detailImage);
-  }
 
-  ngOnDestroy(): void {
-    this.astronomyPicObs.next();
-    this.astronomyPicObs.complete();
   }
 
 }
--- src/app/modules/astronomy-pics/pages/astronomy-pic-detail.page.ts
+++ src/app/modules/astronomy-pics/pages/astronomy-pic-detail.page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AstroPicsService } from '../services/astro-pics.service';
+import { IApd } from '../../../shared/models/apd.model';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   templateUrl: './astronomy-pic-detail.page.html'
 })
-export class AstronomyPicDetailPage {
+export class AstronomyPicDetailPage implements OnInit, OnDestroy {
 
+  detailImage: IApd[] = [];
+  private astronomyPicSubject = new Subject();
+
+  constructor(private astroPicsService: AstroPicsService) {  }
+
+  ngOnInit() {
+    var imageObservable = this.astroPicsService.getDetailImage('2020-12-26', this.detailImage);
+    imageObservable.pipe(takeUntil(this.astronomyPicSubject)).subscribe()
+    console.log(this.detailImage);
+  }
+
+  ngOnDestroy() {
+    this.astronomyPicSubject.next();
+    this.astronomyPicSubject.complete();
+  }
 }
