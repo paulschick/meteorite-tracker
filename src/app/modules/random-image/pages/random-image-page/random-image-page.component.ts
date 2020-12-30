2f784b096326dc947e538f8594eb4a70376d98fa
Paul Schick <pschick@ceiamerica.com>
2020-12-30 15:50:46 -0500

    rendering image from object under notify div

--- src/app/modules/random-image/pages/random-image-page/random-image-page.component.ts
+++ src/app/modules/random-image/pages/random-image-page/random-image-page.component.ts
@@ -12,12 +12,14 @@ export class RandomImagePageComponent implements OnInit {
   randImages = new Array<string>();
   astronomyImgArr:IApd[] = [];
   ASTRONOMY_IMAGE = {
-    "date":"2020-12-30",
-    "explanation":"Yes, but have you seen a movie of Jupiter and Saturn's Great Conjunction? The featured time-lapse video was composed from a series of images taken from Thailand and shows the two giant planets as they angularly passed about a tenth of a degree from each other.  The first Great Conjunction sequence shows a relative close up over five days with moons and cloud bands easily visible, followed by a second video sequence, zoomed out, over 9 days.  Even though Jupiter and Saturn appeared to pass unusually close together on the sky on December 21, 2020, in actuality they were still nearly a billion kilometers apart.  The two gas giants are destined for similar meet ups every 19.86 years.  However, they had not come this close, angularly, for the past 397 years, and will not again for another 60 years.  If you're willing to wait until the year 7541, though, you can see Jupiter  pass directly in front of Saturn.    Gallery: Notable images of the Great Conjunction submitted to APOD",
-    "media_type":"video",
+    "copyright":"Laurie Hatch",
+    "date":"2014-02-28",
+    "explanation":"Only two days past full, February's moon shines through thin clouds, rising on the left in this fisheye night skyscape. The moonlight illuminates a weathered, rounded foreground in the Alabama Hills, conveniently located east of Mt. Whitney along the Sierra Nevada range in California, USA, planet Earth. Orion the Hunter stands at the right, a familiar northern winter constellation. Bright Jupiter, the solar system's ruling gas giant, is near center at the top of the frame. Below Jupiter, Sirius, alpha star of the Big Dog, poses above a bowed and twisted landform known as M\u00f6bius Arch, its curve reminiscent of the mathematically famous surface with only one side. Of course, instead of using rock, wind, and weather, a M\u00f6bius strip is easier to make with paper, scissors, and tape.",
+    "hdurl":"https://apod.nasa.gov/apod/image/1402/LH8056_MobiusArchMoonrise_1024x683.jpg",
+    "media_type":"image",
     "service_version":"v1",
-    "title":"Jupiter and Saturn Great Conjunction: The Movie",
-    "url":"https://www.youtube.com/embed/aokGqxVdpz0?rel=0"
+    "title":"M\u00f6bius Arch Moonrise",
+    "url":"https://apod.nasa.gov/apod/image/1402/LH8056_MobiusArchMoonrise_1024x683.jpg"
   }
 
   constructor() { }
