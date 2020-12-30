673b95eddd270813c7aa8a682d9c7ea61f1ece9a
Paul Schick <pschick@ceiamerica.com>
2020-12-30 15:46:15 -0500

    set up push to array, also made apd model properties optional for development.
    have been having issues with the data from nasa having different parts of the object missin.
    An all optional model isn't great, but if I can get better validation stuff in here then I can tighten it up.

--- src/app/modules/random-image/pages/random-image-page/random-image-page.component.ts
+++ src/app/modules/random-image/pages/random-image-page/random-image-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { of } from 'rxjs';
+import { IApd } from '../../../../shared/models/apd.model';
 
 @Component({
   selector: 'mt-random-image-page',
@@ -9,6 +10,7 @@ import { of } from 'rxjs';
 export class RandomImagePageComponent implements OnInit {
 
   randImages = new Array<string>();
+  astronomyImgArr:IApd[] = [];
   ASTRONOMY_IMAGE = {
     "date":"2020-12-30",
     "explanation":"Yes, but have you seen a movie of Jupiter and Saturn's Great Conjunction? The featured time-lapse video was composed from a series of images taken from Thailand and shows the two giant planets as they angularly passed about a tenth of a degree from each other.  The first Great Conjunction sequence shows a relative close up over five days with moons and cloud bands easily visible, followed by a second video sequence, zoomed out, over 9 days.  Even though Jupiter and Saturn appeared to pass unusually close together on the sky on December 21, 2020, in actuality they were still nearly a billion kilometers apart.  The two gas giants are destined for similar meet ups every 19.86 years.  However, they had not come this close, angularly, for the past 397 years, and will not again for another 60 years.  If you're willing to wait until the year 7541, though, you can see Jupiter  pass directly in front of Saturn.    Gallery: Notable images of the Great Conjunction submitted to APOD",
@@ -28,7 +30,8 @@ export class RandomImagePageComponent implements OnInit {
   ngOnInit(): void {
 
     const imageObservable = of(this.ASTRONOMY_IMAGE);
-    imageObservable.subscribe(data => console.log(data));
+    imageObservable.subscribe((data) => this.astronomyImgArr.push(data));
+    // console.log(this.astronomyImgArr[0])
   }
 
 }
--- src/app/shared/models/apd.model.ts
+++ src/app/shared/models/apd.model.ts
@@ -1,10 +1,10 @@
 export interface IApd {
   copyright?:string;
-  date: string;
-  explanation: string;
-  hdurl: string;
-  media_type: string;
-  service_version: string;
-  title: string;
-  url: string;
+  date?: string;
+  explanation?: string;
+  hdurl?: string;
+  media_type?: string;
+  service_version?: string;
+  title?: string;
+  url?: string;
 }
