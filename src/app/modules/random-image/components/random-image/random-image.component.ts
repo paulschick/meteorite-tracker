41c7a474733ddf9364d9d69e46d601337649aee7
Paul Schick <pschick@ceiamerica.com>
2020-12-30 16:06:50 -0500

    fully functional with dummy Observable

--- src/app/modules/random-image/components/random-image/random-image.component.ts
+++ src/app/modules/random-image/components/random-image/random-image.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'mt-random-image',
@@ -7,7 +8,21 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class RandomImageComponent implements OnInit {
 
-  @Output() notify = new EventEmitter<any>();
+  @Output() postImg = new EventEmitter<any>();
+
+
+  IMG_DATA = {
+    "copyright":"Laurie Hatch",
+    "date":"2014-02-28",
+    "explanation":"Only two days past full, February's moon shines through thin clouds, rising on the left in this fisheye night skyscape. The moonlight illuminates a weathered, rounded foreground in the Alabama Hills, conveniently located east of Mt. Whitney along the Sierra Nevada range in California, USA, planet Earth. Orion the Hunter stands at the right, a familiar northern winter constellation. Bright Jupiter, the solar system's ruling gas giant, is near center at the top of the frame. Below Jupiter, Sirius, alpha star of the Big Dog, poses above a bowed and twisted landform known as M\u00f6bius Arch, its curve reminiscent of the mathematically famous surface with only one side. Of course, instead of using rock, wind, and weather, a M\u00f6bius strip is easier to make with paper, scissors, and tape.",
+    "hdurl":"https://apod.nasa.gov/apod/image/1402/LH8056_MobiusArchMoonrise_1024x683.jpg",
+    "media_type":"image",
+    "service_version":"v1",
+    "title":"M\u00f6bius Arch Moonrise",
+    "url":"https://apod.nasa.gov/apod/image/1402/LH8056_MobiusArchMoonrise_1024x683.jpg"
+  }
+
+  imgObservable = of(this.IMG_DATA)
 
   constructor() { }
 
--- src/app/modules/random-image/pages/random-image-page/random-image-page.component.ts
+++ src/app/modules/random-image/pages/random-image-page/random-image-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { IApd } from '../../../../shared/models/apd.model';
 
 @Component({
@@ -9,31 +9,18 @@ import { IApd } from '../../../../shared/models/apd.model';
 })
 export class RandomImagePageComponent implements OnInit {
 
-  randImages = new Array<string>();
   astronomyImgArr:IApd[] = [];
-  ASTRONOMY_IMAGE = {
-    "copyright":"Laurie Hatch",
-    "date":"2014-02-28",
-    "explanation":"Only two days past full, February's moon shines through thin clouds, rising on the left in this fisheye night skyscape. The moonlight illuminates a weathered, rounded foreground in the Alabama Hills, conveniently located east of Mt. Whitney along the Sierra Nevada range in California, USA, planet Earth. Orion the Hunter stands at the right, a familiar northern winter constellation. Bright Jupiter, the solar system's ruling gas giant, is near center at the top of the frame. Below Jupiter, Sirius, alpha star of the Big Dog, poses above a bowed and twisted landform known as M\u00f6bius Arch, its curve reminiscent of the mathematically famous surface with only one side. Of course, instead of using rock, wind, and weather, a M\u00f6bius strip is easier to make with paper, scissors, and tape.",
-    "hdurl":"https://apod.nasa.gov/apod/image/1402/LH8056_MobiusArchMoonrise_1024x683.jpg",
-    "media_type":"image",
-    "service_version":"v1",
-    "title":"M\u00f6bius Arch Moonrise",
-    "url":"https://apod.nasa.gov/apod/image/1402/LH8056_MobiusArchMoonrise_1024x683.jpg"
-  }
 
   constructor() { }
 
-  receiveNotification(notification: any) {
-    this.randImages.push(notification);
+  postImage(imageObservable:Observable<any>) {
+    imageObservable.subscribe(
+      data => this.astronomyImgArr.push(data)
+    )
   }
 
 
   ngOnInit(): void {
-
-    const imageObservable = of(this.ASTRONOMY_IMAGE);
-    imageObservable.subscribe((data) => this.astronomyImgArr.push(data));
-    // console.log(this.astronomyImgArr[0])
   }
 
 }
