01cc91491350dd4ef7f932cd2cbb037a2a991cb2
Paul <admin@paulschickmarket.com>
2021-01-04 21:40:27 -0500

    directive with increasing value on click

--- nasa-app/1-2021-notes/code-snippets/click-event.directive.ts
+++ nasa-app/1-2021-notes/code-snippets/click-event.directive.ts
@@ -9,3 +9,26 @@
 //     console.log('button was clicked')
 //   }
 // }
+
+// With increasing number on click:
+
+// import { Directive, HostListener } from '@angular/core';
+
+// @Directive({
+//   selector: '[appClickHandler]'
+// })
+// export class ClickHandlerDirective {
+
+//   clickNumber:number;
+//   @HostListener('click', ['$event.target']) onClick() {
+//     console.log('button was clicked')
+//     if (this.clickNumber === undefined || +this.clickNumber === NaN) {
+//       this.clickNumber = 0;
+//       console.log(this.clickNumber);
+//     }
+//     else {
+//       this.clickNumber++
+//       console.log(this.clickNumber);
+//     }
+//   }
+// }
--- nasa-app/src/app/directives/click-handler.directive.ts
+++ nasa-app/src/app/directives/click-handler.directive.ts
@@ -5,7 +5,16 @@ import { Directive, HostListener } from '@angular/core';
 })
 export class ClickHandlerDirective {
 
+  clickNumber:number;
   @HostListener('click', ['$event.target']) onClick() {
     console.log('button was clicked')
+    if (this.clickNumber === undefined || +this.clickNumber === NaN) {
+      this.clickNumber = 0;
+      console.log(this.clickNumber);
+    }
+    else {
+      this.clickNumber++
+      console.log(this.clickNumber);
+    }
   }
 }
