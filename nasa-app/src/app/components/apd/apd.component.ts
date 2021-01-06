91ca487cabbb727b42883be862d529d4fed3f755
Paul Schick <pschick@ceiamerica.com>
2021-01-06 10:09:29 -0500

    spinner implemented on home page

--- nasa-app/src/app/components/apd/apd.component.ts
+++ nasa-app/src/app/components/apd/apd.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IApd } from '../../models/apd.model';
 import { NasaError } from '../../models/nasaErrors.model';
 import { ActivatedRoute } from '@angular/router';
+import { LoaderService } from '../../services/loader.service';
+import { Subscription } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-apd',
   templateUrl: './apd.component.html',
   styleUrls: ['./apd.component.scss']
 })
-export class ApdComponent implements OnInit {
+export class ApdComponent implements OnInit, OnDestroy {
   apd:IApd;
   apdUrl:string;
+  loaderSubscription:Subscription;
+  loading:boolean = true;
 
-  constructor(private route: ActivatedRoute) {  }
+  constructor(private route: ActivatedRoute,
+              private loaderService: LoaderService) {  }
 
   ngOnInit() {
+
+    this.loaderSubscription = this.loaderService.isLoading
+      .pipe(distinctUntilChanged())
+      .subscribe((isLoading) => {
+        console.log(`APD Component Loading: ${isLoading}`);
+        this.loading = isLoading;
+      });
 
     let resolvedApd: IApd | NasaError = this.route.snapshot.data['resolvedApd'];
 
@@ -24,5 +37,9 @@ export class ApdComponent implements OnInit {
       this.apd = resolvedApd[0];
       this.apdUrl = this.apd.url;
     }
+  }
+
+  ngOnDestroy() {
+    this.loaderSubscription.unsubscribe;
   }
 }
