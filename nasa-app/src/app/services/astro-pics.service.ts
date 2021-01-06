6ca634a496cb864b4d246db87524985d8e5bc847
Paul <admin@paulschickmarket.com>
2021-01-05 19:52:39 -0500

    arranged astro-pics service for refactoring

--- nasa-app/src/app/services/astro-pics.service.ts
+++ nasa-app/src/app/services/astro-pics.service.ts
@@ -13,6 +13,10 @@ export class AstroPicsService {
 
   // Refactoring Needed:
   // Concat the endpoints before passing into the GET requests
+
+  // --------------------
+  // Original
+
   private astroPicsUrl: string = 'https://api.nasa.gov/planetary/apod?api_key=';
   private key: string = NASA_API_KEY;
   private queryDate: string = '&date=';
@@ -20,6 +24,48 @@ export class AstroPicsService {
   public formattedDate: string = `${this.myDate.getFullYear()}-${
     this.myDate.getMonth() + 1
   }-${this.myDate.getDate()}`;
+
+  // --------------------
+
+  // --------------------
+  // Original
+
+  private _astroPicsUrl: string = 'https://api.nasa.gov/planetary/apod?api_key=';
+  private _key: string = NASA_API_KEY;
+  private _queryDate: string = '&date=';
+  private _myDate: Date = new Date(Date.now() - 9 * 24 * 60 * 60 * 1000);
+  public _formattedDate: string = `${this._myDate.getFullYear()}-${
+    this._myDate.getMonth() + 1
+  }-${this._myDate.getDate()}`;
+
+  // date range from today
+  // ${this.astroPicsUrl}${this.key}&start_date=${this.formattedDate}`
+  _dateRangeUrl:string = `https://api.nasa.gov/planetary/apod?api_key=${this._key}&start_date=${this._formattedDate}`
+
+  // single image
+  // `${this.astroPicsUrl}${this.key}${this.queryDate}${dateString}`
+  _singleImageUrl:string = `https://api.nasa.gov/planetary/apod?api_key=${this._key}&date=`
+  // use single image in the function just like:
+  // get(`this._singleImageUrl${dateString}`)
+
+  // All I'm doing here is two things:
+  // 1. Better naming
+  // 2. Move url construction the the class, not the method
+
+
+
+
+
+
+
+
+
+  // --------------------
+
+
+
+
+
 
   constructor(private http: HttpClient) {}
 
