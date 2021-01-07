30d651faff570106d278c0a6c298eb25acf33013
Paul <admin@paulschickmarket.com>
2021-01-06 23:19:14 -0500

    removed unused commented code in modules

--- nasa-app/src/app/app-routing.module.ts
+++ nasa-app/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AstronomyPicDetailPageComponent } from './components/astronomy-pic-detail-page/astronomy-pic-detail-page.component';
-// import { HomePageComponent } from './components/home-page/home-page.component';
 import { RandomImagePageComponent } from './components/random-image-page/random-image-page.component';
-// import { ApdResolverService } from './services/apd-resolver.service';
 import { AstronomyPicsResolverService } from './services/astronomy-pics-resolver.service';
 import { MeteoriteResolverService } from './services/meteorite-resolver.service';
 import { MeteoriteContainerComponent } from './components/meteorite-container/meteorite-container.component';
@@ -11,15 +9,10 @@ import { DailyImagesContainerComponent } from './components/daily-images-contain
 
 const routes: Routes = [
   { path: 'astronomy-pics/image/:date', component: AstronomyPicDetailPageComponent },
-  // { path: 'astronomy-pics', component: AstronomyPicsPageComponent, resolve: { resolvedAstronomyPics: AstronomyPicsResolverService } },
   { path: 'astronomy-pics', component: DailyImagesContainerComponent, resolve: { resolvedAstronomyPics: AstronomyPicsResolverService } },
   { path: 'random-image', component: RandomImagePageComponent },
   // development route
   { path: 'new-home', component: MeteoriteContainerComponent, resolve: { resolvedMeteorites: MeteoriteResolverService } },
-  // { path: '', component: HomePageComponent, resolve: {
-  //   resolvedMeteorites: MeteoriteResolverService,
-  //   resolvedApd: ApdResolverService
-  // }, pathMatch: 'full' },
   { path: '', component: MeteoriteContainerComponent, resolve: {
     resolvedMeteorites: MeteoriteResolverService
   }, pathMatch: 'full' },
--- nasa-app/src/app/app.module.ts
+++ nasa-app/src/app/app.module.ts
@@ -12,12 +12,8 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { AstronomyPicDetailComponent } from './components/astronomy-pic-detail/astronomy-pic-detail.component';
 import { AstronomyPicsThumbnailComponent } from './components/astronomy-pics-thumbnail/astronomy-pics-thumbnail.component';
 import { AstronomyPicDetailPageComponent } from './components/astronomy-pic-detail-page/astronomy-pic-detail-page.component';
-// import { ApdComponent } from './components/apd/apd.component';
-// import { MeteoriteDisplayComponent } from './components/meteorite-display/meteorite-display.component';
 import { MeteoriteDetailComponent } from './components/meteorite-detail/meteorite-detail.component';
 import { MeteoriteListComponent } from './components/meteorite-list/meteorite-list.component';
-// import { HomePageComponent } from './components/home-page/home-page.component';
-// import { RandomImageComponent } from './components/random-image/random-image.component';
 import { RandomImagePageComponent } from './components/random-image-page/random-image-page.component';
 
 // Material
@@ -101,12 +97,8 @@ const materialModules = [
     AstronomyPicDetailComponent,
     AstronomyPicsThumbnailComponent,
     AstronomyPicDetailPageComponent,
-    // ApdComponent,
-    // MeteoriteDisplayComponent,
     MeteoriteDetailComponent,
     MeteoriteListComponent,
-    // HomePageComponent,
-    // RandomImageComponent,
     RandomImagePageComponent,
     ObserveBreakpointsDirective,
     ClickHandlerDirective,
