6a7c39ade52665539e9f9da88f664b7067a339d2
Paul Schick <pschick@ceiamerica.com>
2021-01-11 16:23:08 -0500

    all routes functional with refactored DataService

--- nasa-app/src/app/components/astronomy-pic-detail-page/astronomy-pic-detail-page.component.ts
+++ nasa-app/src/app/components/astronomy-pic-detail-page/astronomy-pic-detail-page.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AstroPicsService } from '../../services/astro-pics.service';
+// import { AstroPicsService } from '../../services/astro-pics.service';
 import { IApd } from '../../models/apd.model';
 import { NasaError } from '../../models/nasaErrors.model';
+import { NASA_API_KEY } from '../../configs/nasa-config';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-astronomy-pic-detail-page',
   templateUrl: './astronomy-pic-detail-page.component.html'
 })
 export class AstronomyPicDetailPageComponent implements OnInit {
-
+  key:string = NASA_API_KEY;
   detailImages: IApd[] = [];
 
-  constructor(private astroPicsService: AstroPicsService,
+  constructor(private dataService:DataService,
               private route: ActivatedRoute) {  }
 
   ngOnInit() {
 
     const imageDate = this.route.snapshot.paramMap.get('date')
+    const detailImageUrl = `https://api.nasa.gov/planetary/apod?api_key=${this.key}&date=${imageDate}`;
 
-    this.astroPicsService.getDetailImage(imageDate).subscribe(
+    this.dataService.getRequest(detailImageUrl, false).subscribe(
       (data: IApd) => (this.detailImages.push(data)),
       (err: NasaError) => console.log(err),
       () => console.log('finished retrieving single Astronomy Picture from Nasa')
