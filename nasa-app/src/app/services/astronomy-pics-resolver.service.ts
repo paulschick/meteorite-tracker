ca4cfbb2191271188157e7faefc82d0d7b1e3654
Paul Schick <pschick@ceiamerica.com>
2021-01-11 16:19:24 -0500

    meteorites and astronomy date range images all working with new req

--- nasa-app/src/app/services/astronomy-pics-resolver.service.ts
+++ nasa-app/src/app/services/astronomy-pics-resolver.service.ts
@@ -4,17 +4,25 @@ import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { NasaError } from '../models/nasaErrors.model';
 import { IApd } from '../models/apd.model';
-import { AstroPicsService } from './astro-pics.service';
+// import { AstroPicsService } from './astro-pics.service';
+import { DataService } from './data.service';
+import { NASA_API_KEY } from '../configs/nasa-config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AstronomyPicsResolverService implements Resolve<IApd[] | NasaError> {
+  private key: string = NASA_API_KEY;
+  private tenDaysPrior: Date = new Date(Date.now() - 9 * 24 * 60 * 60 * 1000);
+  public formattedDate: string = `${this.tenDaysPrior.getFullYear()}-${
+    this.tenDaysPrior.getMonth() + 1
+  }-${this.tenDaysPrior.getDate()}`;
+  dateRangeUrl: string = `https://api.nasa.gov/planetary/apod?api_key=${this.key}&start_date=${this.formattedDate}`;
 
-  constructor(private astroPicsService: AstroPicsService) {  }
+  constructor(private dataService:DataService) {  }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IApd[] | NasaError> {
-    return this.astroPicsService.getFromDateRange()
+    return this.dataService.getRequest(this.dateRangeUrl, false)
       .pipe(
         catchError(err => of(err))
       );
--- nasa-app/src/app/services/meteorite-resolver.service.ts
+++ nasa-app/src/app/services/meteorite-resolver.service.ts
@@ -5,17 +5,19 @@ import { catchError } from 'rxjs/operators';
 
 import { IMeteorite } from '../models/meteorite.model';
 import { NasaError } from '../models/nasaErrors.model';
-import { AstroPicsService } from './astro-pics.service';
+// import { AstroPicsService } from './astro-pics.service';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeteoriteResolverService implements Resolve<IMeteorite[] | NasaError> {
+  meteoritesUrl: string = 'https://data.nasa.gov/resource/gh4g-9sfh.json';
 
-  constructor(private astroPicsService: AstroPicsService) {  }
+  constructor(private dataService: DataService) {  }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IMeteorite[] | NasaError> {
-    return this.astroPicsService.getAllMeteorites()
+    return this.dataService.getRequest(this.meteoritesUrl, false)
       .pipe(
         catchError(err => of(err))
       );
