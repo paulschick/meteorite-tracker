040358062c7a3f5995abc4990b128053abfdba03
Paul Schick <pschick@ceiamerica.com>
2021-01-19 07:21:51 -0500

    multiple functions passes sorting tests
    slight refactor, utilize multiple functions, can use switch statement to determine which one is to be executed based on the object that is passed in. Can test that as well when developed

--- nasa-app/src/app/services/sort.service.spec.ts
+++ nasa-app/src/app/services/sort.service.spec.ts
@@ -1,9 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SortService } from './sort.service';
+import { SortService, ITempInterface } from './sort.service';
 
 describe('SortService', () => {
   let service: SortService;
+  let myArray:ITempInterface[] = [
+    { prop:'1' },
+    { prop:'2' },
+    { prop:'4' },
+    { prop:'8' },
+    { prop:'9' },
+  ];
+  let myStringArray:ITempInterface[] = [
+    { prop:'a' },
+    { prop:'e' },
+    { prop:'r' },
+    { prop:'z' },
+    { prop:'b' },
+  ];
+  let prop:string = 'prop';
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,4 +28,49 @@ describe('SortService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should sort by object property of numbers descending', () => {
+    expect(service.sortPropDesc(myArray,prop)).toEqual(
+      [
+        { prop:'9' },
+        { prop:'8' },
+        { prop:'4' },
+        { prop:'2' },
+        { prop:'1' },
+      ]
+    )
+  })
+  it('should sort by object property of numbers ascending', () => {
+    expect(service.sortPropAsc(myArray,prop)).toEqual(
+      [
+        { prop:'1' },
+        { prop:'2' },
+        { prop:'4' },
+        { prop:'8' },
+        { prop:'9' },
+      ]
+    )
+  })
+  it('should sort by object property of strings descending', () => {
+    expect(service.sortStringPropsDesc(myStringArray,prop)).toEqual(
+      [
+        { prop:'z' },
+        { prop:'r' },
+        { prop:'e' },
+        { prop:'b' },
+        { prop:'a' },
+      ]
+    )
+  })
+  it('should sort by object property of strings descending', () => {
+    expect(service.sortStringPropsAsc(myStringArray,prop)).toEqual(
+      [
+        { prop:'a' },
+        { prop:'b' },
+        { prop:'e' },
+        { prop:'r' },
+        { prop:'z' },
+      ]
+    )
+  })
 });
--- nasa-app/src/app/services/sort.service.ts
+++ nasa-app/src/app/services/sort.service.ts
@@ -18,20 +18,23 @@ export class SortService {
     return arr.sort((a,b) => +b.prop - +a.prop);
   }
 
-  sortPropAsc(arr:ITempInterface[],prop:string):ITempInterface[] {
+  sortPropAsc(arr:ITempInterface[],prop:string):ITempInterface[]
+  {
     return arr.sort((a,b) => +a.prop - +b.prop);
   }
   //*---------------------------------
 
   //* Sort string Asc/Desc
   //? Uses: Meteorite -> Name
-  sortStringPropsDesc(arr:ITempInterface[],prop:string):ITempInterface[] {
+  sortStringPropsDesc(arr:ITempInterface[],prop:string):ITempInterface[]
+  {
     return arr.sort((a,b) => {
       return b.prop > a.prop ? 1 : b.prop === a.prop ? 0 : -1
     });
   }
 
-  sortStringPropsAsc(arr:ITempInterface[],prop:string):ITempInterface[] {
+  sortStringPropsAsc(arr:ITempInterface[],prop:string):ITempInterface[]
+  {
     return arr.sort((a,b) => {
       return a.prop > b.prop ? 1 : a.prop === b.prop ? 0 : -1
     });
