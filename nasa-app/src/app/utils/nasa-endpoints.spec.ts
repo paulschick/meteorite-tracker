8863b9f36f21e9b5ac4a78ff07a773f1cca56f0b
Paul Schick <pschick@ceiamerica.com>
2021-01-20 02:59:06 -0500

    updated tests

--- nasa-app/src/app/utils/nasa-endpoints.spec.ts
+++ nasa-app/src/app/utils/nasa-endpoints.spec.ts
@@ -1,11 +1,9 @@
-import { GetDateRange } from './date-methods';
 import { NASA_API_KEY } from '../configs/nasa-config';
 import { ENDPOINT } from './nasa-endpoints';
 
 describe('nasa-endpoints', () => {
 
   let _endpoint: ENDPOINT;
-  let getDateRange: GetDateRange;
   let key: string;
 
   beforeEach(() => {
@@ -13,8 +11,7 @@ describe('nasa-endpoints', () => {
   });
 
   it('url should include the correct date and API key', () => {
-    _endpoint = new ENDPOINT(10);
-    // expect(_endpoint.DATE_RANGE).toContain('2021-1-10');
+    _endpoint = new ENDPOINT();
     expect(_endpoint.DATE_RANGE).toContain(key);
   });
 });
